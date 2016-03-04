fad56a75b1b35220515b5e0bf7570191563d46ef
Daniel Glover <gloversdaniel@gmail.com>
2016-03-04 16:30:40 -0700

    glover's branch upload

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source 'https://rubygems.org'
 
+gem 'pry'
+gem 'better_errors'
+
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5.2'
--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -1,10 +1,46 @@
 class AgentsController < ApplicationController
+  
+
   def index
+    @agents = Agent.all
   end
 
   def show
   end
 
-  def new
+  def edit
+    @agent = Agent.find(params[:id])
   end
+
+  def new
+    @agent = Agent.new
+  end
+
+  def update
+    if @agent.update(agent_params[:id])
+      redirect_to
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @agent = Agent.new(agent_params)
+    if @agent.save
+      redirect_to agents_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+    def agent_params
+      params.require(:agent).permit(:name, :phone, :email)
+    end
+
+    def agent
+      @agent = Agent.find(params[:id])
+    end
 end
