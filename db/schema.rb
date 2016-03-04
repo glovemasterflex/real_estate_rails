3d49433d66fc5455cd6aff90bd04c8a37150f96a
Justin Ewalt <justinewalt@gmail.com>
2016-03-04 14:21:15 -0700

    Created foundation

---
+++ db/schema.rb
@@ -0,0 +1,49 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160304205927) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string   "street", null: false
+    t.string   "city", null: false
+    t.string   "state", null: false
+    t.string   "zip", null: false
+    t.integer  "homes_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "agents", force: :cascade do |t|
+    t.string   "name", null: false
+    t.string   "phone", null: false
+    t.string   "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "homes", force: :cascade do |t|
+    t.integer  "bed",  null: false
+    t.integer  "bath", null: false
+    t.integer  "sqft", null: false
+    t.integer  "price", null: false
+    t.boolean  "sold"
+    t.boolean  "like"
+    t.integer  "agents_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
